c8454024173536eb57aa8581a729656539afc2a5
Moon <mjskks94@naver.com>
2022-10-10 13:40:41 +0900

    feat : 기능개발 요구사항
    시도 횟수 입력받는 기능 개발
    - 시도 횟수 입력받는 기능 개발
    - 횟수 숫자 여부 검증 기능 개발

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,16 +8,24 @@ import java.util.Map;
 public class Application {
 
     private static boolean retry = false;
+    private static int retryCnt = 0;
     private static Map<String,String> cars;
 
     public static void main(String[] args) {
         cars = new HashMap<>();
         startRacingGame();
         getValidCarInput();
+
+        getTryCount();
+        getValidTryCountInput();
     }
 
     protected static void startRacingGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    protected static void getTryCount() {
+        System.out.println("시도할 횟수는 몇회인가요?");
     }
 
     protected static void getValidCarInput() {
@@ -37,9 +45,29 @@ public class Application {
         }
     }
 
+    protected static void getValidTryCountInput() {
+        String tryCount = Console.readLine();
+        retry = false;
+
+        try {
+            tryConutValidation(tryCount);
+            retryCnt = Integer.parseInt(tryCount);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]" + e.getMessage());
+        } finally {
+            tryInputRetry();
+        }
+    }
+
     protected static void carInputRetry() {
         if (retry) {
             getValidCarInput();
+        }
+    }
+
+    protected static void tryInputRetry() {
+        if (retry) {
+            getValidTryCountInput();
         }
     }
 
@@ -53,6 +81,13 @@ public class Application {
         if (!retry && car.length() > 5) {
             retry = true;
             throw new IllegalArgumentException("5자 이하만 가능하다.");
+        }
+    }
+
+    protected static void tryConutValidation(String tryCount) {
+        if (!tryCount.chars().allMatch(Character::isDigit)) {
+            retry = true;
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 한다.");
         }
     }
 }
